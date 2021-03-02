405f0035d040a4999abd46e6f009599b33b06919
DanielDO93 <danieldionisio1993@gmail.com>
2021-03-02 13:33:56 -0600

    Update _nav.js

--- src/_nav.js
+++ src/_nav.js
@@ -6,19 +6,84 @@ export default {
       class: 'text-center',
     },
     {
-      name: 'Reglas ortográficas de la V',
+      name: 'Importancia de la ortografía',
       icon: 'icon-tag',
      
     },
 
    
     {
-      name: 'Evaluación 1',
+      name: 'Los acentos',
       icon: 'icon-puzzle',
     
     },
     {
-      name: 'Evaluación 2',
+      name: 'La letra B',
+      icon: 'icon-puzzle',
+    
+    },
+    {
+      name: 'La letra V',
+      icon: 'icon-puzzle',
+    
+    },
+    {
+      name: 'La letra C',
+      icon: 'icon-puzzle',
+    
+    },
+    {
+      name: 'La letra S',
+      icon: 'icon-puzzle',
+    
+    },
+    {
+      name: 'La letra Z',
+      icon: 'icon-puzzle',
+    
+    },
+    {
+      name: 'La letra G',
+      icon: 'icon-puzzle',
+    
+    },
+    {
+      name: 'La letra J',
+      icon: 'icon-puzzle',
+    
+    },
+    {
+      name: 'La letra H',
+      icon: 'icon-puzzle',
+    
+    },
+    {
+      name: 'La letra LL',
+      icon: 'icon-puzzle',
+    
+    },
+    {
+      name: 'La letra Y',
+      icon: 'icon-puzzle',
+    
+    },
+    {
+      name: 'La letra N y la letra M',
+      icon: 'icon-puzzle',
+    
+    },
+    {
+      name: 'La letra CC',
+      icon: 'icon-puzzle',
+    
+    },
+    {
+      name: 'La letra SC',
+      icon: 'icon-puzzle',
+    
+    },
+    {
+      name: 'Función cumplen los signos de puntuación',
       icon: 'icon-puzzle',
     
     },
